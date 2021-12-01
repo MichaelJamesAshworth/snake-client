3fad132bd64d980fcc599e8ef58be57666dfa475
Michael <Michael.James.Ashworth@Gmail.com>
2021-12-01 23:38:58 +0000

    played around with writing commands to implement upon connection to server

--- client.js
+++ client.js
@@ -9,18 +9,18 @@ const connect = function () {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    console.log("Successfully connected to game server");
-  });
   
   conn.on("data", (data) => {
     console.log(data);
   });
-
-  conn.on("connect", () => {
-    conn.write("Name: MJA");
-  });
   
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: MJA");
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 50);
+  });
   return conn;
 };
 
